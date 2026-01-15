9da27abfb6e6bd6c890acdebf77a77e81160ac76
OrcDev <urosmiric88@gmail.com>
2026-01-15 16:33:27 +0100

    refactor(routes) remove unused project routes for cleaner API structure

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,8 @@
 import { Hono } from "hono";
-import previouslySubmittedRouter from "./previously-submitted-projects.js";
 import projectsRouter from "./projects.js";
-import reviewedProjectsRouter from "./reviewed-projects.js";
 
 const apiRoutes = new Hono();
 
 apiRoutes.route("/projects", projectsRouter);
-apiRoutes.route("/reviewed-projects", reviewedProjectsRouter);
-apiRoutes.route("/previously-submitted-projects", previouslySubmittedRouter);
 
 export default apiRoutes;
